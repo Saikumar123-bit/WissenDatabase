a9c045b230dd7f53c0c105b1bf06acd05c56c192
V SAIKUMAR <SecondPerson@gmail.com>
2020-04-17 16:35:51 +0530

    ActionDatabase has been modified

--- AjaxConnectToDatabase/src/com/wissen/action/ActionDatabase.java
+++ AjaxConnectToDatabase/src/com/wissen/action/ActionDatabase.java
@@ -1,5 +1,13 @@
 package com.wissen.action;
 
+import java.util.Date;
+
 public class ActionDatabase {
 
+	public Date getSysdate() {
+		return new Date();
+	}
+	public void m1() {
+		System.out.println("'Action Database");
+	}
 }
